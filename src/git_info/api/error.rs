03acc70b2665ecc9c8d37669a61b5395f2895337
juanjo <juanjo@juanjofp.com>
2024-11-13 19:29:16 +0100

    Improve ApiError enum

--- src/git_info/api/error.rs
+++ src/git_info/api/error.rs
@@ -1,77 +1,78 @@
 use std::fmt::Display;
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum ApiServiceErrorKind {
-    NoResponse,
-    NoBody,
-    InvalidJson,
-    NotFound,
-    FieldNotFound(String),
+pub enum ApiError {
+    NoResponse { message: String },
+    NoBody { message: String },
+    InvalidJson { message: String },
+    NotFound { message: String },
+    FieldNotFound { message: String, field: String },
 }
 
-impl ApiServiceErrorKind {
-    fn message(&self) -> String {
+impl ApiError {
+    pub fn no_response(message: Option<String>) -> Self {
+        let message = message.unwrap_or("No response from the server".to_string());
+
+        ApiError::NoResponse { message }
+    }
+
+    pub fn no_body(message: Option<String>) -> Self {
+        let message = message.unwrap_or("No body in the response".to_string());
+
+        ApiError::NoBody { message }
+    }
+
+    pub fn invalid_json(message: Option<String>) -> Self {
+        let message = message.unwrap_or("Invalid JSON in the response".to_string());
+
+        ApiError::InvalidJson { message }
+    }
+
+    pub fn not_found(message: Option<String>) -> Self {
+        let message = message.unwrap_or("Resource not found".to_string());
+
+        ApiError::NotFound { message }
+    }
+
+    pub fn field_not_found(field: &str, message: Option<String>) -> Self {
+        let message = message.unwrap_or(format!("Field {} not found", field));
+
+        ApiError::FieldNotFound {
+            message,
+            field: field.to_string(),
+        }
+    }
+
+    pub fn message(&self) -> &str {
         match self {
-            ApiServiceErrorKind::NoResponse => String::from("Server ERROR, no response"),
-            ApiServiceErrorKind::NoBody => String::from("No body"),
-            ApiServiceErrorKind::InvalidJson => String::from("Error invalid json"),
-            ApiServiceErrorKind::NotFound => String::from("Error user not found"),
-            ApiServiceErrorKind::FieldNotFound(field) => format!("Error {} not found", field),
+            ApiError::NoResponse { message } => message,
+            ApiError::NoBody { message } => message,
+            ApiError::InvalidJson { message } => message,
+            ApiError::NotFound { message } => message,
+            ApiError::FieldNotFound { message, .. } => message,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        match &self {
+            ApiError::NoResponse { .. } => "NoResponse",
+            ApiError::NoBody { .. } => "NoBody",
+            ApiError::InvalidJson { .. } => "InvalidJson",
+            ApiError::NotFound { .. } => "NotFound",
+            ApiError::FieldNotFound { .. } => "FieldNotFound",
         }
     }
 }
 
-#[derive(Clone, Debug, PartialEq)]
-pub struct ApiError {
-    kind: ApiServiceErrorKind,
-    message: String,
-}
+impl std::error::Error for ApiError {}
 
 impl Display for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "ApiError: status: {}, message: {:?}",
-            self.kind.message(),
-            self.message
+            "ApiError: status: {}, message: {}",
+            self.name(),
+            self.message(),
         )
-    }
-}
-
-impl ApiError {
-    pub fn new(kind: ApiServiceErrorKind, message: Option<String>) -> Self {
-        let message = message.unwrap_or_else(|| kind.message());
-        ApiError { kind, message }
-    }
-
-    pub fn no_response(message: Option<String>) -> Self {
-        ApiError::new(ApiServiceErrorKind::NoResponse, message)
-    }
-
-    pub fn no_body(message: Option<String>) -> Self {
-        ApiError::new(ApiServiceErrorKind::NoBody, message)
-    }
-
-    pub fn invalid_json(message: Option<String>) -> Self {
-        ApiError::new(ApiServiceErrorKind::InvalidJson, message)
-    }
-
-    pub fn not_found(message: Option<String>) -> Self {
-        ApiError::new(ApiServiceErrorKind::NotFound, message)
-    }
-
-    pub fn field_not_found(field: &str, message: Option<String>) -> Self {
-        ApiError::new(
-            ApiServiceErrorKind::FieldNotFound(field.to_string()),
-            message,
-        )
-    }
-
-    pub fn kind(&self) -> &ApiServiceErrorKind {
-        &self.kind
-    }
-
-    pub fn message(&self) -> &str {
-        self.message.as_ref()
     }
 }
